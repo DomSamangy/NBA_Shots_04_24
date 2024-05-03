346ae3e93943b2ce820a3ffa20269f021fb6eacb
levaegor <eg.work.me@gmail.com>
2024-05-03 11:30:06 +0300

    Improved annotations

--- numerical_characteristics.py
+++ numerical_characteristics.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import sympy as sp
-import matplotlib.pyplot as plt
 import math
 
 
@@ -17,42 +16,52 @@ class DiscreteCharacteristics:
 
 
     def validate_parameters(self):
-        if(self.value_array_length!= self.probability_array_length):
+        if self.value_array_length!= self.probability_array_length:
             raise ValueError('ERROR: array lengths must be equal to each other')
-        if (sum(self.probability) < 0.9999 or sum(self.probability) > 1.0001):
+        if 0.9999 <= sum(self.probability) <= 1.0001:
             raise ValueError('ERROR: probability sum must be equal to 1')
 
     
-    def raw_moment(self, power: int, digits_to_round: int): #начальный момент k-ого порядка
-        sum = 0
+
+class Discrete1D(DiscreteCharacteristics):
+    def __init__(self, value, probability):
+        DiscreteCharacteristics.__init__(self, value, probability)
+
+
+    def raw_moment(self, n: int, digits_to_round: int = 2) -> float: 
+        """ Returns the n-th raw moment """
+        sum: float = 0
         for i in range(self.value_array_length):
-            sum += self.probability[i]*self.value[i]**power   
+            sum += self.probability[i]*self.value[i]**n   
         return round(sum, digits_to_round)
 
-    
-    def math_expectancy(self, argument: list, digits_to_round: int): #математическое ожидание
-        sum = 0
-        if (self.probability_array_length == len(argument)):
-            for i in range(self.value_array_length):
-                sum += self.probability[i]*argument[i] 
-            return round(sum, digits_to_round)
-        else:
-            raise ValueError('ERROR: array lengths must be equal to each other')
+
+    def math_expectancy(self, digits_to_round: int = 2) -> float:
+        """ Returns expectancy """
+        return self.raw_moment(1, digits_to_round)
+
 
     
-    def central_moment(self, power: int, digits_to_round: int): #центральный момент k-ого порядка
-        sum = 0
+    def central_moment(self, n: int, digits_to_round: int = 2) -> float: 
+        """ Returns the n-th central moment """
+        sum: float = 0
         for i in range(self.value_array_length):
-            sum += self.probability[i]*(self.value[i]-self.math_expectancy(self.value, digits_to_round))**power   
-        sum = round(sum, digits_to_round)
-        return sum
+            sum += self.probability[i]*(self.value[i]-self.math_expectancy(digits_to_round))**n   
+        return round(sum, digits_to_round)
 
 
-    def standard_deviation(self, digits_to_round: int): #среднеквадратичное отклонение
-        return round(math.sqrt(self.central_moment(2, digits_to_round)), digits_to_round)
+    def dispersion(self, digits_to_round: int = 2) -> float:
+        """ Returns dispersion """
+        return self.central_moment(2, digits_to_round)
+
+
+    def standard_deviation(self, digits_to_round: int): 
+        """ Returns the standard deviation """
+        return round(math.sqrt(self.dispersion(digits_to_round)), digits_to_round)
     
     
-    def moment_generating_function(self):#производящая функция моментов
+    def moment_generating_function(self) -> sp.core.add.Add:
+        """ Returns a moment-generating function """
         t = sp.Symbol('t')
         sum = 0
         for i in range(self.value_array_length):
@@ -60,7 +69,8 @@ class DiscreteCharacteristics:
         return sum
     
     
-    def characteristic_function(self): #характеристическая функция 
+    def characteristic_function(self) -> sp.core.add.Add: 
+        """ Returns a characteristic function """
         t = sp.Symbol('t')
         sum = 0
         for i in range(self.value_array_length):
@@ -68,7 +78,8 @@ class DiscreteCharacteristics:
         return sum
     
     
-    def raw_moment_from_mgf(self, n: int, digits_to_round: int): #начальный момент через производящую функцию моментов
+    def raw_moment_from_mgf(self, n: int, digits_to_round: int) -> float: 
+        """ Returns the n-th raw moment by deriving the moment-generating function """
         t = sp.Symbol('t')
         mgf = self.moment_generating_function()
         der = sp.diff(mgf, t, n)
@@ -76,7 +87,8 @@ class DiscreteCharacteristics:
         return round(moment(0), digits_to_round)
     
     
-    def raw_moment_from_cf(self, n: int, digits_to_round: int): #начальный момент через характеристическую функцию 
+    def raw_moment_from_cf(self, n: int, digits_to_round: int) -> float: 
+        """ Returns the n-th raw moment by deriving the characteristic function """
         t = sp.Symbol('t')
         mgf = self.characteristic_function()
         der = (sp.I**(-n))*sp.diff(mgf, t, n)
@@ -84,15 +96,18 @@ class DiscreteCharacteristics:
         return round(float(sp.re(moment(0))), digits_to_round)
     
     
-    def skewness(self, digits_to_round: int): #коэффициент асимметрии
+    def skewness(self, digits_to_round: int) -> float: 
+        """ Returns skewness """
         return round(self.central_moment(3, digits_to_round)/(self.standard_deviation(digits_to_round)**3), digits_to_round)
     
     
-    def kurtosis(self, digits_to_round: int): #коэффициент экцесса
+    def kurtosis(self, digits_to_round: int) -> float: 
+        """ Returns kurtosis """
         return round(self.central_moment(4, digits_to_round)/(self.standard_deviation(digits_to_round)**4), digits_to_round)
 
 
-    def cdf(self): #функция распределения
+    def cdf(self) -> list[np.ndarray, list]: 
+        """ Returns the values of cumulative distribution function """
         y_critical_points = []
         sum = 0
         for x in range(len(self.value)):
@@ -117,18 +132,12 @@ class DiscreteCharacteristics:
         return [x_axis, y_axis]
         
 
+        
+        
 
-
-
+if __name__ == '__main__':
+    tmp = Discrete1D([1, 2, 3], [0.5, 0.3, 0.2])
     
-
-        
-
-        
-        
-
-# tmp = DiscreteCharacteristics([1, 2, 3], [0.1, 0.6, 0.3])
-# cdf = tmp.cdf()
-# print(cdf)
+    print(len)
 
 
